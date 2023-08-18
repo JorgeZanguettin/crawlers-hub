404f2312ed1b9e0849f1082acafaa3523d20fae9
Jorge Zanguettin <jorgelzbr@gmail.com>
2023-08-18 16:56:19 +0100

    Add Crawler Idealista

---
+++ crawlers/idealista/README.md
@@ -0,0 +1,38 @@
+# **Steam**
+
+## O que é Steam?
+
+Steam é um software de gestão de direitos digitais criado pela Valve Corporation ou Valve L.L.C., de plataformas digitais como jogos e aplicativos de programação e fornece serviços facilitados como atualização automática de jogos, e preços acessíveis aos usuários.
+
+## **Dados Coletados**
+
+## Variaveis
+
+```yaml
+{
+title : String,
+url : String,
+price : Float,
+typology : String,
+area : String,
+description : String,
+}
+```
+
+## Amostra
+
+| title | url | price | typology | area | description |
+|---|---|---|---|---|---|
+| Apartamento T0 na rua Giovanni Antinori, 2, Rio Seco - Casalinho, Ajuda | https://www.idealista.pt/imovel/31052742/ | 900€/mês | T0 | 55 m² área bruta | MARAVILHOSO ESTÚDIO COMO NOVO localizado ao pé... |
+| Apartamento T2, Calçada da Ajuda, Calçada da Ajuda - Belém, Ajuda | https://www.idealista.pt/imovel/32831581/ | 2.950€/mês | T2 | 94 m² área bruta | "Apartamento T2 inserido, inserido no prestigiado... |
+| Apartamento T2 em Boa Hora, Ajuda | https://www.idealista.pt/imovel/32644962/ | 1.600€/mês | T2 | 78 m² área bruta | Venha conhecer este excelente T2+1, acabado de remodelar e pronto a estrear... |
+| Duplex, Calçada do Mirante, Alto da Ajuda, Ajuda | https://www.idealista.pt/imovel/32774575/ | 1.890€/mês | T2 | 130 m² área bruta | T2 duplex mobilado, com vista para o Palácio da Ajuda e o Tejo... |
+| Apartamento T4 em Boa Hora, Ajuda | https://www.idealista.pt/imovel/32582590/ | 3.700€/mês | T4 | 170 m² área bruta | Apartamento com vista maravilhosa sobre o rio... |
+
+
+## Como executar?
+
+1. Faça o download e instalação do Python 3 [Aqui](https://www.python.org/).
+2. Direcione o seu bash de preferencia a pasta raiz do repositorio e instale as dependencias utilizando o comando ```pip install -r requirements.txt``` ou ```pip3 install -r requirements.txt``` .
+3. Execute o Crawler com o comando ```python idealista.py``` ou ```python3 idealista.py``` .
+4. Pronto! Os dados coletados serão armazenados em um arquivo CSV na raiz do projeto com o nome **imoveis.csv**.
---
+++ crawlers/idealista/idealista.py
@@ -0,0 +1,90 @@
+import os
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+
+URL_CRAWL = "https://www.idealista.pt/arrendar-casas/lisboa/"
+
+
+class CrawlerIdealista:
+    def __init__(self):
+        page = 1
+        self.send_log_system('DEGUB', 'Start Crawler')
+
+        while True:
+            page = self.parse_place_list(page)
+            if not page:
+                break
+
+        self.send_log_system('DEGUB', 'Finish Crawler')
+
+
+    def parse_place_list(self, page):
+        self.send_log_system('REQUEST', URL_CRAWL + f"pagina-{page}")
+
+        place_request = requests.get(
+            URL_CRAWL + f"pagina-{page}",
+            headers={
+                "accept": "application/json, text/javascript, */*; q=0.01",
+                "accept-language": "pt-BR,pt;q=0.9",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+            }
+        )
+        soup = BeautifulSoup(place_request.content, "html.parser")
+
+        for place in soup.select("main#main-content article.item.extended-item.item-multimedia-container"):
+            place_json = {
+                "title": self.string_process(place, "div.item-info-container a.item-link"),
+                "url": self.string_process(place, "div.item-info-container a.item-link", link=True),
+                "price": self.string_process(place, "span.item-price.h2-simulated"),
+                "typology": self.string_process(place, "div.item-detail-char span:nth-of-type(1)"),
+                "area": self.string_process(place, "div.item-detail-char span:nth-of-type(2)"),
+                "description": self.string_process(place, "div.item-description.description p")
+            }
+            self.send_log_system('DEGUB', place_json["url"])
+
+            self.save_to_csv(place_json)
+
+        if not soup.select_one("a.icon-arrow-right-after"):
+            return None
+        return page + 1
+
+    def string_process(self, place, string, link=False):
+        if string:
+            if not link:
+                return str(place.select_one(string).text).replace("\n", "").strip()
+            else:
+                return "https://www.idealista.pt" + str(place.select_one(string)["href"]).strip()
+        return ""
+    
+
+    def send_log_system(self, type_message, message):
+        print (f'[ {type_message} ] {message}')
+
+
+    def save_to_csv(self, place):
+        if "imoveis.csv" not in os.listdir():
+            with open('imoveis.csv', 'w', encoding='utf-8', newline="") as csv_file:
+                csvWriter = csv.writer(csv_file, delimiter=';')
+                csvWriter.writerow([
+                    "title",
+                    "url",
+                    "price",
+                    "typology",
+                    "area",
+                    "description"
+                ])
+        
+        with open('imoveis.csv', 'a+', encoding='utf-8', newline="") as csv_file:
+            csvWriter = csv.writer(csv_file, delimiter=';')
+            csvWriter.writerow([
+                place["title"],
+                place["url"],
+                place["price"],
+                place["typology"],
+                place["area"],
+                place["description"]
+            ])
+
+CrawlerIdealista()
