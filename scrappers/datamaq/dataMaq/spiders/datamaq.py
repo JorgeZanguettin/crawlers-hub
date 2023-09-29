3dd71f13bcd266ccbb608823981992506ba559f9
JorgeZanguettin <jorgelzbr@gmail.com>
2023-09-29 10:43:41 +0100

    Update Spider methodology

--- scrappers/datamaq/dataMaq/spiders/datamaq.py
+++ scrappers/datamaq/dataMaq/spiders/datamaq.py
@@ -1,8 +1,6 @@
 import scrapy
-import requests
 import urllib.parse
 from ..items import DatamaqItem
-from bs4 import BeautifulSoup
 
 class DatamaqSpider(scrapy.Spider):
     name = 'datamaq'
@@ -10,58 +8,106 @@ class DatamaqSpider(scrapy.Spider):
     custom_settings = {
         'ITEM_PIPELINES': {
             'dataMaq.pipelines.DatamaqPipeline': 400
+        },
+        "DOWNLOADER_MIDDLEWARES": {
+            "dataMaq.middlewares.DatamaqSpiderMiddleware": 400,
+            "dataMaq.middlewares.ScrapyTelegramBotMiddleware": 405
         }
     }
-    
-    obj_bs4 = BeautifulSoup(requests.get('http://www.datamaq.org.br/SearchResult/AdditionalFilter?parentId=undefined&sectorName=undefined&isSegment=undefined').content, 'html.parser')
-    array_urls = []
-    for obj_url in obj_bs4.select('input.btn.btn-outline-info'):
-        if 'showChildSector' in str(obj_url['onclick']):       
-            id_categoria = str(obj_url['onclick']).replace("showChildSector('", "").replace("');", "").replace("', '", ",").split(',')[0]
-            categoria = str(obj_url['onclick']).replace("showChildSector('", "").replace("');", "").replace("', '", ",").split(',')[1]
 
-            url_categoria = 'http://www.datamaq.org.br/SearchResult/AccountIndustrialSectorList?industrialSectorId={}&sectorName={}&isSegment=undefined'.format(
-                id_categoria,
-                urllib.parse.quote_plus(categoria)
-            )
-
-            array_urls.append(url_categoria)
-
-    start_urls = array_urls
+    start_urls = [
+        'http://www.datamaq.org.br/SearchResult/AdditionalFilter?parentId=undefined&sectorName=undefined&isSegment=undefined'
+    ]
 
     def parse(self, response):
-        for empresa_block in response.css("table.table.table-sm.table-hover tbody tr::attr(onclick)").extract():
-            yield scrapy.Request(self.getUrlEmpresa(empresa_block), callback=self.getEmpresaDetails, dont_filter=True)
+        replace_characters = [
+            ("showChildSector('", ""),
+            ("');", ""),
+            ("', '", ","),
+        ]
 
-    def getEmpresaDetails(self, response):
+        for obj_url in response.css('input.btn.btn-outline-info::attr(onclick)').extract():
+            if 'showChildSector' in str(obj_url):
+                obj_child = obj_url
+                for r in replace_characters:
+                    obj_child = obj_child.replace(
+                        r[0], r[1]
+                    )
+                obj_child = obj_child.split(",")
+
+                id_categoria = obj_child[0]
+                categoria = obj_child[1]
+
+                url_categoria = 'http://www.datamaq.org.br/SearchResult/AccountIndustrialSectorList?industrialSectorId={}&sectorName={}&isSegment=undefined'.format(
+                    id_categoria,
+                    urllib.parse.quote_plus(categoria)
+                )
+
+                yield scrapy.Request(
+                    url=url_categoria,
+                    callback=self.parse_category
+                )
+
+    def parse_category(self, response):
+        for empresa_block in response.css("table.table.table-sm.table-hover tbody tr::attr(onclick)").extract()[0:1]:
+            yield scrapy.Request(
+                url=self.get_url_companies(empresa_block),
+                callback=self.parse_company_details,
+                dont_filter=True
+            )
+
+    def parse_company_details(self, response):
+        replace_characters = [
+            ":",
+            "º",
+            " ",
+            "-"
+        ]
+
         self.item_empresa = DatamaqItem()
         
         self.item_empresa['UrlEmpresa'] = response.url
         for block_prop in response.css("div.container div.row"):
             field_name = block_prop.css("div.col-2 span::text").extract_first()
-            if field_name is not None:
-                field_name = str(field_name).replace(':', '').replace('º', '').replace(' ', '').replace('-', '').strip()
-                field_value = block_prop.css("div.col span::text").extract_first()
 
-                if field_value is None:
-                    field_value = block_prop.css("div.col a::text").extract_first()
-                    if field_value is not None:
-                        field_value = str(field_value).strip()
-                else:
+            if not field_name:
+                continue
+
+            for r in replace_characters:
+                field_name = field_name.replace(
+                    r[0], ""
+                )
+
+            field_value = block_prop.css("div.col span::text").extract_first()
+
+            if field_value:
+                field_value = str(field_value).strip()
+            else:
+                field_value = block_prop.css("div.col a::text").extract_first()
+
+                if field_value:
                     field_value = str(field_value).strip()
+                else:
+                    continue
 
-                self.item_empresa[field_name] = field_value
+            self.item_empresa[field_name] = field_value
 
-        yield scrapy.Request(self.getUrlProdutos(response.xpath('/html/body/div[3]/div[1]/input/@onclick').extract_first()), callback=self.getProdutosDetails, dont_filter=True)
+        yield scrapy.Request(
+            url=self.get_url_products(
+                response.xpath('/html/body/div[3]/div[1]/input/@onclick').extract_first()
+            ),
+            callback=self.parse_product_details,
+            dont_filter=True
+        )
 
-    def getProdutosDetails(self, response):
+    def parse_product_details(self, response):
         self.item_empresa['Produtos'] = response.css('table.table.table-sm.table-hover tbody tr td a::text').extract()
 
         yield self.item_empresa
 
-    def getUrlProdutos(self, empresa_block):
+    @staticmethod
+    def get_url_products(empresa_block):
         propriedades = str(empresa_block).replace('openLineOfProduction(', '').replace(');', '').replace("'", "").split(', ')
-        print(empresa_block)
 
         return 'http://www.datamaq.org.br/SearchResult/LineOfProduction?sectorId={}&sectorName={}&entityId={}&entityIDName={}&isSector={}'.format(
             propriedades[0],
@@ -71,8 +117,10 @@ class DatamaqSpider(scrapy.Spider):
             propriedades[4],
         )
 
-    def getUrlEmpresa(self, empresa_block):
+    @staticmethod
+    def get_url_companies(empresa_block):
         propriedades_empresa = str(empresa_block).replace('openAccountDetail(', '').replace(');', '').replace("'", "").split(', ')
+
         return 'http://www.datamaq.org.br/SearchResult/ShowManufacturer?sectorId={}&sectorName={}&entityId={}&entityIDName={}&sourceType=2&industrialInstallationProductId=&isSector=1'.format(
             propriedades_empresa[0],
             urllib.parse.quote_plus(propriedades_empresa[1]),
